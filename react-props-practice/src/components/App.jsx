6dba9cd5dd98f24207e93cdca1bd6de89decfbf6
Sean Huang <hyprul@ucla.edu>
2019-11-29 23:26:18 -0600

    apps

--- react-props-practice/src/components/App.jsx
+++ react-props-practice/src/components/App.jsx
@@ -5,6 +5,7 @@ import contacts from '../contacts';
 function App() {
   return (
     <div>
+      <h1 className="heading">My Contacts</h1>
       {contacts.map(Contact =>
         <Card name={Contact.name} imgURL={Contact.imgURL} phone={Contact.phone} email={Contact.email} />)}
     </div>
--- react-props-practice/src/components/Card.jsx
+++ react-props-practice/src/components/Card.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
 
-export default function Card(props){
-    return(
-        <div className="Card">
-        <h1 className="heading">My Contacts</h1>
+export default function Card(props) {
+    return (
+
         <div className="card">
-          <div className="top">
-            <h2 className="name">{props.name}</h2>
-            <img className="circle-img"
-              src={props.imgURL}
-              alt="avatar_img"
-            />
-          </div>
-          <div className="bottom">
-            <p>{props.phone}</p>
-            <p>{props.email}</p>
-          </div>
+            <div className="top">
+                <h2 className="name">{props.name}</h2>
+                <img className="circle-img"
+                    src={props.imgURL}
+                    alt="avatar_img"
+                />
+            </div>
+            <div className="bottom">
+                <p className="info">{props.phone}</p>
+                <p className="info">{props.email}</p>
+            </div>
         </div>
-      </div>
+
     )
 }
